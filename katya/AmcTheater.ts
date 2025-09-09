c63f2a22e983865251099d3b4e8b37d42a3b7b1d
kolesnikovak <katyamiller36@gmail.com>
2025-09-08 20:50:56 -0500

    AMCTheater class created, 1 method added (#24)
    * AMCTheater class created, 1 method added
    
    * added data types for variables

---
+++ katya/AmcTheater.ts
@@ -0,0 +1,9 @@
+class AMCTheaterHomePage{
+
+    homePageMessage : string = 'See more.Earn More'
+    percentageOfDiscount : number = 50
+
+    clickingComingSoonButton() : void{
+        console.log('Directing to coming soon page...')
+    }
+}
