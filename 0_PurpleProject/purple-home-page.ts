12d98949666c9235586a36b703bd549472b68fba
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-09 18:40:18 -0500

    created a class with variables

---
+++ 0_PurpleProject/purple-home-page.ts
@@ -0,0 +1,13 @@
+
+class PurpleHomepagePage {
+    heroTitle = "The Purple Mattress";
+    shopNowButton: string = "#shop-now-btn";
+    navigationMenu: string = ".main-nav";
+    clickShopNow(): void { 
+        console.log("Do you wanna add it to cart?");
+    }
+    navigateToMattresses(): void { 
+        console.log("What kind of the mattresses you are looking for");
+    }
+}
+  
