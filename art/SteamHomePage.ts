94ccf3076f1530dfc85e2d79122a6273e2e3a447
artemN63 <nikishynartem@gmail.com>
2025-09-08 20:26:40 -0500

    new Steam Home Page balance method

---
+++ art/SteamHomePage.ts
@@ -0,0 +1,12 @@
+class SteamHomePage {
+
+    expectedBalanceMessage = 'Add Funds'
+    expectedAmountMessage = 100
+
+    addFundsButton(): void {
+        console.log('Clicking ' + this.expectedBalanceMessage 
+            + ' button and choosing ' + this.expectedAmountMessage 
+            + '$ option')
+    }
+
+}
