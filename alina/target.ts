4c654e22aefc23d5e5323a889d01b5562617e732
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-08 20:29:49 -0500

    added variable and changed 't' to uppercase

--- alina/target.ts
+++ alina/target.ts
@@ -1,4 +1,5 @@
-class targetHomePage {
+class TargetHomePage {
+  welcomeMessage = "Shop more, spend less";
   goToCart(items: number): number {
     let cartItems = 5;
     cartItems = cartItems + items;
