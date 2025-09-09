2df5dce1afc6322033a86baca1e1106466c3b5de
kolesnikovak <katyamiller36@gmail.com>
2025-09-08 20:33:59 -0500

    new class created, new method added (#16)
    * new class created, new method added
    
    * empty space deleted

---
+++ katya/SpiritHomePage2.ts
@@ -0,0 +1,9 @@
+class SpiritHomePage{
+
+    homePageTitle = 'More Places - More Faces'
+    numberOfPoints = 13567
+
+    loginMessage():void{
+        console.log('Loged-in succesfully')
+    }
+}
