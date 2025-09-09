2d71615b618fd3004c8a631b9251fb7feb971197
thotska <thotska@gmail.com>
2025-09-08 19:54:51 -0500

    AddedOneMoreVariable

--- tania/ClassAppleTicket.ts
+++ tania/ClassAppleTicket.ts
@@ -1,7 +1,8 @@
 class AppleWebSite{
     expectedTitle: string = "Meet the expected Iphone 16"
+    expectedButton: string = "Buy Now"
     navigateToHomePage(): void{
         console.log("Join Iphone 16 presentation")
     }
-    
+
 }
