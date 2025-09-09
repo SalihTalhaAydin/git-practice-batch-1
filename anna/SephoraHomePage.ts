1ea6f17a10eaaff29c6b9854ee6caeba250d13fa
amakarenko1203 <annamakarenko.rdh@gmail.com>
2025-09-08 19:34:20 -0500

    added a new code for Sephora
    Checkout feature, and add to box

---
+++ anna/SephoraHomePage.ts
@@ -0,0 +1,10 @@
+class SephoraHomnPage {
+    expectedMessage = "New week, new yoy, Anna."
+
+    chekcout(): void{
+        console.log('You can checkout now')
+    }
+    addToBox(): void{
+        console.log('Add new item to the box')
+    }
+}
