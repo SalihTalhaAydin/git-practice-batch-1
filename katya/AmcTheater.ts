b0f59e68a143d3f625c8f47f466c29f693fc9bdb
kolesnikovak <katyamiller36@gmail.com>
2025-09-08 20:48:46 -0500

    added data types for variables

--- katya/AmcTheater.ts
+++ katya/AmcTheater.ts
@@ -1,7 +1,7 @@
 class AMCTheaterHomePage{
 
-    homePageMessage = 'See more.Earn More'
-    percentageOfDiscount = 50
+    homePageMessage : string = 'See more.Earn More'
+    percentageOfDiscount : number = 50
 
     clickingComingSoonButton() : void{
         console.log('Directing to coming soon page...')
