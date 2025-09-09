bfea79719ba010b77f3db3b7738b2cd29f1217d7
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-08 20:48:14 -0500

    created a class with method (#22)
    * created a class with method
    
    * created a new variable

---
+++ alina/AmazonHomePage.ts
@@ -0,0 +1,7 @@
+class AmazonHomePage {
+  expectedMessage = "Shop the deals";
+  recommendedDealForYou = "50% off Apple Headphones";
+  goToSearchString(): void {
+    console.log("Search ot ask a question");
+  }
+}
