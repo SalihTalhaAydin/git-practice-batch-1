f466469d00db7c94471e9b450fcdcdbaf366aa57
artemN63 <nikishynartem@gmail.com>
2025-09-08 19:27:38 -0500

    added Apple Home Page
    1. created a class Apple Home Page
    2. created 2 expected variables for test
    3. created 1 method to press button "Add to calendar"

---
+++ art/AppleHomePage.ts
@@ -0,0 +1,12 @@
+class AppleHomePage {
+
+    expectedGreetMessage = "Apple Event Watch tomorrow at 10 a.m. PT."
+    expectedButtonMessage = "Add to calendar"
+
+    pressAddToCalendarButton(): void {
+
+        console.log('Pressing "' + this.expectedButtonMessage + '" button...')
+
+    }
+
+}
