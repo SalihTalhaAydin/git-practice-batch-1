7ec3163efa26fb931da3521a9e4e8716a2ec6b9c
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-08 19:33:58 -0500

    created a class and added  variables

---
+++ alina/gittest.ts
@@ -0,0 +1,8 @@
+class GitPractise { 
+    expectedTitle = 'welcome to our practise';
+    levelOfUnderstanding = 'average'
+    navigateToPage(): void { 
+        console.log('Yay you are here');
+    }
+
+}
