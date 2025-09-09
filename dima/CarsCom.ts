69c30e3474f79e90a001f8b510dc5afd7e9d6ee2
freeeams <freams.fox@gmail.com>
2025-09-08 19:48:53 -0500

    Update CarsCom.ts

--- dima/CarsCom.ts
+++ dima/CarsCom.ts
@@ -1,5 +1,7 @@
 class CarsCom {
     titleHomePage:string ='Imagine the possibilities'
     searchTitle : string = 'SUVs under 60k$'
-    
+    searchACar(): void{
+        console.log('Searching the car')
+    }
 }
