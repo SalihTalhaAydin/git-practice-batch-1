d33c5d7ed9d2732bc5dcd8de70281be06bec12bd
kolesnikovak <katyamiller36@gmail.com>
2025-09-09 18:38:59 -0500

    created Purple Account class, added variables and methods

---
+++ katya/purple-account-page.ts
@@ -0,0 +1,17 @@
+//👤 Purple Account Page - Katya
+//File: purple-account-page.ts
+//Branch: add-account-page
+//Variables: profileSection: string = ".profile-info", orderHistory: string = ".order-list", saveButton: string = ".save-profile"
+//Methods: updateProfile(), viewOrderHistory()
+
+class PurpleAccountPage{
+profileSection: string = ".profile-info"
+orderHistory: string = ".order-list"
+saveButton: string = ".save-profile"
+
+    updateProfile() : void{
+    }
+
+    viewOrderHistory() : void {
+    }
+}
