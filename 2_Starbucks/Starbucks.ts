71b71738fb0c3da031334686ee4d506603a665da
freeeams <freams.fox@gmail.com>
2025-09-10 18:21:02 -0500

    StarbucksDima

---
+++ 2_Starbucks/Starbucks.ts
@@ -0,0 +1,10 @@
+
+
+
+class Starbucks {
+  expectedMessage = "Hot chocolate,lemonade and more";
+  recommendedDealForYou = "Enjoy coffee";
+  goToSearchString(): void {
+    console.log("Search or ask a question");
+  }
+}
