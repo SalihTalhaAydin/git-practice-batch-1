e0185185e6afd7e04ee71daf5c66a560a6dc9b3e
vovan-u-s <oversoul6661@gmail.com>
2025-09-09 20:40:36 -0500

    NextClassForHP

--- 1_HpProject/HpTestPage.ts
+++ 1_HpProject/HpTestPage.ts
@@ -5,11 +5,3 @@ class HpHomePage{
     }
 }
 
-class HpCartPage{
-titleMessageInsideTheCart:string='Cart is empty'
- addingStuffToTheCart(subject:string):void{
-    console.log(subject)
-    return
- }
-
-}
