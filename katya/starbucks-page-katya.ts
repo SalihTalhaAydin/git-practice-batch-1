b4778be3d9e223b134a62a61193c7c1900d8423d
kolesnikovak <katyamiller36@gmail.com>
2025-09-10 18:23:47 -0500

    StarbucksPage created, 1 method added

---
+++ katya/starbucks-page-katya.ts
@@ -0,0 +1,9 @@
+class StarbucksPage{
+
+    starbucksPageMessage : string = "Fall. Starts. Now."
+    starbucksRewardsMessage : string = "Just join Starbucks® Rewards and enjoy a free drink with a qualifying purchase during your first week.*"
+
+    startAnOrderButton() : void{
+        console.log("Clicking on the start an order button")
+    }
+}
