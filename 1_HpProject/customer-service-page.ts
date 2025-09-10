ca9c5561eb995651ddb6c467743e561a839de502
artemN63 <nikishynartem@gmail.com>
2025-09-09 20:50:30 -0500

    create customer service page (#60)
    * create customer service page
    
    * fixed

---
+++ 1_HpProject/customer-service-page.ts
@@ -0,0 +1,13 @@
+class CustomerServicePage {
+
+    expectedMainTitle = 'Need order help? Find fast, self-serve solutions now!'
+    expectedLowerTitle = 'Check your order status, request a return or cancellation, and report a delivery issue.'
+
+    pressOrderStatusButton(): void {
+        console.log('Pressing Order status button...')
+    }
+
+    pressRequestAReturnButton(): void {
+        console.log('Pressing Request a return button...')
+    }
+}
