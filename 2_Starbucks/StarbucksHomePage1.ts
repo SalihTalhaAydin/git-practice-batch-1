9fc6cbfe3088ca0431ef51e4e7f9855cbd19b378
freeeams <freams.fox@gmail.com>
2025-09-10 18:45:02 -0500

    StarbucksTask (#87)

---
+++ 2_Starbucks/StarbucksHomePage1.ts
@@ -0,0 +1,8 @@
+
+class Starbucks1{
+  expectedMessage = "Hot chocolate,lemonade and more";
+  recommendedDealForYou = "Enjoy coffee";
+  goToSearchString(): void {
+    console.log("Search or ask a question");
+  }
+}
