1d412f932b7861056c1095908bbe70cba66cff9b
thotska <thotska@gmail.com>
2025-09-10 18:34:12 -0500

    MovedStarbucksCode

---
+++ 2_Starbucks/StarbucksMainPaige.ts
@@ -0,0 +1,7 @@
+class StarbucksMainPAige{
+    expectedHomepageMessage: string = "Fall. Starts. Now."
+
+    clickViewMenu():void{
+        console.log("Our fall flavors are officially here.Time for all things pumpkin and pecan, including the new Pecan Oatmilk Cortado.")
+    }
+}
