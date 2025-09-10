e0ce09bbb5ff044b93c40879067bb6dfb3f69120
kolesnikovak <katyamiller36@gmail.com>
2025-09-10 17:41:19 -0500

    HomePage class created, 1 method added

---
+++ katya/homepage.ts
@@ -0,0 +1,8 @@
+class HomePage{
+    homePageMessage : string = "Welcome Back!"
+    numberOfSubscriptions : number = 287
+
+    navigateToTaabs(): void{
+        console.log("Navigating to tabs...")
+    }
+}
