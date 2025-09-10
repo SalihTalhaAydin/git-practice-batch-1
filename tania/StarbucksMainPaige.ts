8862f32e653293ca0cbe2d67606c2375bfe0b1f8
thotska <thotska@gmail.com>
2025-09-10 18:38:21 -0500

    DeletedStarbucksCodeFromPersonalFolder

--- tania/StarbucksMainPaige.ts
+++
@@ -1,7 +0,0 @@
-class StarbucksMainPAige{
-    expectedHomepageMessage: string = "Fall. Starts. Now."
-
-    clickViewMenu():void{
-        console.log("Our fall flavors are officially here.Time for all things pumpkin and pecan, including the new Pecan Oatmilk Cortado.")
-    }
-}
