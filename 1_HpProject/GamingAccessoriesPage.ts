1388420290bc8f8a7ff3140bd9a965d3541f53f1
kolesnikovak <katyamiller36@gmail.com>
2025-09-09 20:42:20 -0500

    GamingAccessoriesPage class created, new methods added

---
+++ 1_HpProject/GamingAccessoriesPage.ts
@@ -0,0 +1,13 @@
+class GamingAccessoriesPage{
+
+    automaticalyChoosenProducts : string []= ["Monitors", "Accessories"]
+    amountOfResults : number = 163
+
+    inStockFilter(){
+        console.log("Showing all of the items in stock...")
+    }
+
+    addToCart(){
+        console.log("Item was added successfully!")
+    }
+}
