5a8592849c6fe216c4c6745784c10907f610c9a2
artemN63 <nikishynartem@gmail.com>
2025-09-09 20:36:45 -0500

    Create Monitors and Accessories page (#57)

---
+++ 1_HpProject/monitors-and-accessories-page.ts
@@ -0,0 +1,14 @@
+class MonitorsAndAccessoriesPage {
+
+    expectedMainTitle = 'HP Monitors & Accessories'
+    expectedShopButtonMessage = 'SHOP DEALS'
+
+    pressMonitorsButton(): void {
+        console.log('Pressing Monitors button...')
+    }
+
+    pressBundlesButton(): void {
+        console.log('Pressing Bundles button...')
+    }
+
+}
