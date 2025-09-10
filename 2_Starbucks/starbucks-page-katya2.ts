a5908ec6a7ffb57a5c79e6adeafa0f5ed91ca545
kolesnikovak <katyamiller36@gmail.com>
2025-09-10 18:38:22 -0500

    Recreated Starbucks class under the Starbucks Folder

---
+++ 2_Starbucks/starbucks-page-katya2.ts
@@ -0,0 +1,7 @@
+class StarbucksPage2{
+ starbucksPageMessage : string = "Fall. Starts. Now."
+ starbucksRewardsMessage : string = "Just join Starbucks® Rewards and enjoy a free drink with a qualifying purchase during your first week.*"
+ startAnOrderButton() : void{
+        console.log("Clicking on the start an order button")
+    }
+}
