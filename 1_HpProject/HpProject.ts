ac11c8c236408a686a5f033de8dd7d762213a96c
freeeams <freams.fox@gmail.com>
2025-09-09 20:49:13 -0500

    HPPROJECT

--- 1_HpProject/HpProject.ts
+++
@@ -1,7 +0,0 @@
-class HPHomePage {
-  expectedMessage = "Shop the deals";
-  recommendedDealForYou = "50% off HP Laptop";
-  goToSearchString(): void {
-    console.log("Search ot ask a question");
-  }
-}
---
+++ 1_HpProject/HpProject2.ts
@@ -0,0 +1,7 @@
+class HPHomePage {
+  expectedMessage = "Shop the deals";
+  recommendedDealForYou = "50% off HP Laptop";
+  goToSearchString(): void {
+    console.log("Search ot ask a question");
+  }
+}
