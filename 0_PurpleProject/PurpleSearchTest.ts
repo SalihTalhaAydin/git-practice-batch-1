883582ee164d2259baf529daabfbdc3eac30f777
vovan-u-s <oversoul6661@gmail.com>
2025-09-09 19:15:47 -0500

    New-purple-search-branch
    im replace it to purple progect

---
+++ 0_PurpleProject/PurpleSearchTest.ts
@@ -0,0 +1,12 @@
+class PurpleSearchPages{
+    searchBox: string = "#search-input"
+    searchButton: string = ".search-btn"
+    resultsContainer: string = ".search-results"
+     enterSearchTerm(term: string):string{
+        console.log(term + 'found')
+        return term
+     }
+     clickSearchButton():void{
+        console.log('Push the button')
+     }
+}
