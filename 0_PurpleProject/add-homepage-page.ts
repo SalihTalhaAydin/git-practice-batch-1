5faf65c6d50cd946e64ffd5563fe17ee39a7bf0b
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-09 19:36:06 -0500

    added a new branch in the right folder (#50)
    * created a class with variables
    
    * moved to my folder
    
    * moved to my folder
    
    * created a new branch
    
    * no spaces
    
    * moved to purple project
    
    * deleted

---
+++ 0_PurpleProject/add-homepage-page.ts
@@ -0,0 +1,11 @@
+class PurpleHomepageePage {
+  heroTitle = "The Purple Mattress";
+  shopNowButton: string = "#shop-now-btn";
+  navigationMenu: string = ".main-nav";
+  clickShopNow(): void {
+    console.log("Do you wanna add it to cart?");
+  }
+  navigateToMattresses(): void {
+    console.log("What kind of the mattresses you are looking for");
+  }
+}
