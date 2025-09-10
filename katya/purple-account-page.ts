9e9628689dee676bcb9c7f00bb984f81204db8fa
kolesnikovak <katyamiller36@gmail.com>
2025-09-09 19:21:14 -0500

    Deleted comments

--- katya/purple-account-page.ts
+++ katya/purple-account-page.ts
@@ -1,9 +1,3 @@
-//👤 Purple Account Page - Katya
-//File: purple-account-page.ts
-//Branch: add-account-page
-//Variables: profileSection: string = ".profile-info", orderHistory: string = ".order-list", saveButton: string = ".save-profile"
-//Methods: updateProfile(), viewOrderHistory()
-
 class PurpleAccountPage{
 profileSection: string = ".profile-info"
 orderHistory: string = ".order-list"
