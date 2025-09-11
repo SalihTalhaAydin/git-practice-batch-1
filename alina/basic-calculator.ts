2a33ae7d0c9f7955b59c6ceca92daa18c4b45807
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-10 22:34:03 -0500

    added calculator

---
+++ alina/basic-calculator.ts
@@ -0,0 +1,12 @@
+class Calculator { 
+    number1 = 5;
+    number2 = 10;
+    number3 = 20;
+    addNumbers(number1: number, number2: number): number { 
+        let sum = number1 + number2;
+
+        console.log("The sum is ", sum);
+        return sum;
+    }
+
+}
